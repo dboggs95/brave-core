0107bcecd4144a7c06303b23664fb75a031a04b6
sangwoo.ko <sangwoo108@gmail.com>
2022-06-10 12:42:30 +0900

    [Experiment] Vertical tabs
    This new feature is at experimental stage for now. So it's hidden
    behind flag('Vertical tabs')
    
    Implmented:
    * Expand and collapse via a button on top of tabs.
    * Vertical scroll area for tabs
    * TabCards
    * Tab reordering via drag-and-drop
    * Tab detaching and attaching via drag-and-drop
    * Shape customization
    
    TBD:
    * Switching tabs layout direction on runtime
      - need to plumb pref to many UI components.
    * Title area in frame
      - Show title or move up toolbar
      - Moving up toolbar can take some time.
    * Layout animation

---
+++ chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
@@ -0,0 +1,45 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/views/tabs/brave_tab_container.h"
+#include "brave/browser/ui/views/tabs/brave_vertical_tab_utils.h"
+
+#define TabContainer BraveTabContainer
+#define BRAVE_TAB_STRIP_CALCULATE_INDEX_FOR_VERTICAL_TABS                      \
+  if (ShouldShowVerticalTabs()) {                                              \
+    const int ideal_y =                                                        \
+        candidate_index == 0 ? 0 : ideal_bounds(candidate_index - 1).bottom(); \
+    const int distance = std::abs(dragged_bounds.y() - ideal_y);               \
+    if (distance < min_distance) {                                             \
+      min_distance = distance;                                                 \
+      min_distance_index = candidate_index;                                    \
+    }                                                                          \
+    continue;                                                                  \
+  }
+
+#define BRAVE_TAB_STRIP_INIT_DRAG_CONTROLLER_FOR_VERTICAL_TABS             \
+  if (ShouldShowVerticalTabs())                                            \
+    drag_controller_->Init(this, source, dragging_views, gfx::Point(x, y), \
+                           event.y(), std::move(selection_model),          \
+                           EventSourceFromEvent(event));                   \
+  else  // NOLINT(readability/braces)
+
+#define BRAVE_TAB_STRIP_CALCULATE_DRAGGED_VIEW_BOUNDS_FOR_VERTICAL_TABS \
+  if (ShouldShowVerticalTabs()) {                                       \
+    std::vector<gfx::Rect> bounds;                                      \
+    int y = 0;                                                          \
+    for (const TabSlotView* view : views) {                             \
+      const int height = view->height();                                \
+      bounds.push_back(gfx::Rect(0, y, view->width(), view->height())); \
+      y += height;                                                      \
+    }                                                                   \
+    return bounds;                                                      \
+  }
+
+#include "src/chrome/browser/ui/views/tabs/tab_strip.cc"
+#undef BRAVE_TAB_STRIP_CALCULATE_DRAGGED_VIEW_BOUNDS_FOR_VERTICAL_TABS
+#undef BRAVE_TAB_STRIP_INIT_DRAG_CONTROLLER_FOR_VERTICAL_TABS
+#undef BRAVE_TAB_STRIP_CALCULATE_INDEX_FOR_VERTICAL_TABS
+#undef TabContainer
